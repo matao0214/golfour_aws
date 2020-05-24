41bc2dbf46b441be06992ee401d70602f945e59c
matao0214 <y.kawamata0214@gmail.com>
2020-05-24 21:03:03 +0900

    ログイン実行機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
