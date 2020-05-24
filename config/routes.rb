5abdb0196686c58cfa8b24c356d5185805bc1979
matao0214 <y.kawamata0214@gmail.com>
2020-05-24 21:17:57 +0900

    bin/rails g controller Users new edit showを実行

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/edit'
+  get 'users/show'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
