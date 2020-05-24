1314500ea88ab104df75454161ea8a085dc22507
matao0214 <y.kawamata0214@gmail.com>
2020-05-24 21:28:28 +0900

    Usersコントローラにアクション追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,44 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
+    @user = User.new
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
-  def show
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to user_url(@user), notice: "ユーザー「#{@user.nickname}」を登録しました。"
+    else
+      render :new
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_url(@user), notice: "ユーザー「#{@user.nickname}」を更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  # def destroy
+  #   @user = User.find(params[:id])
+  #   @user.destroy
+  #   redirect_to user_url(@user), notice: "ユーザー「#{@user.nickname}」を削除しました。"
+  # end
+
+  private
+    
+  def user_params
+    params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :golf_reki, :goal)
   end
 end
