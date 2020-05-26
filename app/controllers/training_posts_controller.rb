26c570b026e8a4c4b3fcd3b5ac25c6f2846d5df9
matao0214 <y.kawamata0214@gmail.com>
2020-05-26 13:51:13 +0900

    createアクションを作成

--- app/controllers/training_posts_controller.rb
+++ app/controllers/training_posts_controller.rb
@@ -9,6 +9,19 @@ class TrainingPostsController < ApplicationController
     @training_post = TrainingPost.new
   end
 
+  def create
+    @training_post = current_user.training_posts.new(training_post_params)
+    @training_post.save!
+    redirect_to root_url, notice: "投稿しました。" 
+  end
+
   def edit
+  end
+
+
+private
+
+  def training_post_params
+    params.require(:training_post).permit(:training_place, :training_advance, :training_task, :training_impression)
   end
 end
