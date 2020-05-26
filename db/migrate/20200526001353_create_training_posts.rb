89e91820cc69bdf3782fab56ba7d24f12dd067f4
matao0214 <y.kawamata0214@gmail.com>
2020-05-26 09:53:59 +0900

    制約を追加してマイグレーション

--- db/migrate/20200526001353_create_training_posts.rb
+++ db/migrate/20200526001353_create_training_posts.rb
@@ -1,6 +1,11 @@
 class CreateTrainingPosts < ActiveRecord::Migration[5.2]
   def change
     create_table :training_posts do |t|
+      t.string :training_place, limit: 50, null: false
+      t.text :training_advance, limit: 150, null: false
+      t.text :training_task, limit: 150, null: false
+      t.text :training_impression, limit: 150
+      t.references :user, foreign_key: true, null: false
 
       t.timestamps
     end
