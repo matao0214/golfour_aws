1ae11bc1f26599ac6835431165ca4b6748a146b2
matao0214 <y.kawamata0214@gmail.com>
2020-06-03 10:05:54 +0900

    修正

--- app/controllers/training_posts_controller.rb
+++ app/controllers/training_posts_controller.rb
@@ -53,13 +53,4 @@ private
   def correct_training_post
     redirect_to root_path if !current_user.own_post?(@training_post)
   end
-
-  # def correct_training_post
-  #   redirect_to root_path if current_user.id != @training_post.user_id
-  # end
-
-  
-  
-
-
 end
