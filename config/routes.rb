c3db5eb830ba7f22fa3f8ce131b8321f330ca8ce
matao0214 <y.kawamata0214@gmail.com>
2020-06-05 11:42:42 +0900

    ゲストユーザーログイン機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :users, except: :index
 
   get '/top', to: 'top#index'
+  post '/guest_login', to: 'sessions#guest_login'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
