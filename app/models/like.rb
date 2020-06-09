e3d695c8eb62013b7626a2ff5fc013d9fddfd341
matao0214 <y.kawamata0214@gmail.com>
2020-06-09 10:34:37 +0900

    likeモデルのバリデーションの書き方を変更

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,5 @@ class Like < ApplicationRecord
   belongs_to :training_post
   belongs_to :user
 
-  # validates_uniqueness_of :training_post_id, scope: :user_id
   validates :training_post_id, uniqueness: { scope: :user_id }
 end
