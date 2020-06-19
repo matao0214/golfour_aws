9a0a80de892902c5b86400ddc9119973bba9d118
matao0214 <y.kawamata0214@gmail.com>
2020-06-19 11:09:17 +0900

    meta-tagsを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'GolfOur',
+      separator: '-',
+      title: 'ゴルフ初心者専用投稿アプリ-GolfOur-',
+      charset: 'utf-8',
+      description: 'ゴルフをこれから始めたい方。ゴルフを始めたけど挫折しそうな方。同じ悩みを持つ仲間を見つけてゴルフを楽しみましょう！',
+      canonical: 'https://golfour.herokuapp.com/'
+    }
+  end
 end
