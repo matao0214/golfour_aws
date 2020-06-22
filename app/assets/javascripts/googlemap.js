60e19e96722a882f319eda83d9726958f029c860
matao0214 <y.kawamata0214@gmail.com>
2020-06-22 13:59:20 +0900

    地図表示に合わせてレイアウトを修正

--- app/assets/javascripts/googlemap.js
+++ app/assets/javascripts/googlemap.js
@@ -1,41 +1,41 @@
-let map //変数の定義
-let geocoder //変数の定義
+let map
+let Geocoder
 
-function initMap(){ //コールバック関数
-  geocoder = new google.maps.Geocoder() //GoogleMapsAPIジオコーディングサービスにアクセス
-  if(document.getElementById('map')){ //'map'というidを取得できたら実行
-    map = new google.maps.Map(document.getElementById('map'), { //'map'というidを取得してマップを表示
-      center: {lat: 35.6594666, lng: 139.7005536}, //最初に表示する場所（今回は「渋谷スクランブル交差点」が初期値）
-      zoom: 15, //拡大率（1〜21まで設定可能）
+function initMap(){
+  geocoder = new google.maps.Geocoder()
+  if(document.getElementById('map')){
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 35.6594666, lng: 139.7005536},
+      zoom: 15,
     });
-  }else{ //'map'というidが無かった場合
-    map = new google.maps.Map(document.getElementById('show_map'), { //'show_map'というidを取得してマップを表示
-      center: {lat: gon.lat, lng: gon.lng}, //controllerで定義した変数を緯度・経度の値とする（値はDBに入っている）
-      zoom: 15, //拡大率（1〜21まで設定可能）
+  }else{ 
+    map = new google.maps.Map(document.getElementById('show_map'), {
+      center: {lat: gon.lat, lng: gon.lng},
+      zoom: 15,
     });
 
-    marker = new google.maps.Marker({ //GoogleMapにマーカーを落とす
-      position:  {lat: gon.lat, lng: gon.lng}, //マーカーを落とす位置を決める（値はDBに入っている）
-      map: map //マーカーを落とすマップを指定
+    marker = new google.maps.Marker({
+      position:  {lat: gon.lat, lng: gon.lng},
+      map: map
     });
   }
 }
 
-function codeAddress(){ //コールバック関数
-  let inputAddress = document.getElementById('address').value; //'address'というidの値（value）を取得
+function codeAddress(){
+  let inputAddress = document.getElementById('address').value;
 
-  geocoder.geocode( {'address': inputAddress}, function(results, status) { //ジオコードしたい住所を引数として渡す
+  geocoder.geocode( {'address': inputAddress}, function(results, status) {
     if (status == 'OK') {
-      let lat = results[0].geometry.location.lat(); //ジオコードした結果の緯度
-      let lng = results[0].geometry.location.lng(); //ジオコードした結果の経度
+      let lat = results[0].geometry.location.lat();
+      let lng = results[0].geometry.location.lng();
       let mark = {
-          lat: lat, //緯度
-          lng: lng  //経度
+          lat: lat,
+          lng: lng
       };
-      map.setCenter(results[0].geometry.location); //最も近い、判読可能な住所を取得したい場所の緯度・経度
+      map.setCenter(results[0].geometry.location);
       let marker = new google.maps.Marker({
-          map: map, //マーカーを落とすマップを指定
-          position: results[0].geometry.location //マーカーを落とす位置を決める
+          map: map,
+          position: results[0].geometry.location
       });
     } else {
       alert('該当する結果がありませんでした');
