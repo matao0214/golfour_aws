40ebe9e472f8f7cfa30821c204be487019b880b6
matao0214 <y.kawamata0214@gmail.com>
2020-06-25 09:58:28 +0900

    バリデーションを追加

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -2,7 +2,7 @@ class Spot < ApplicationRecord
   belongs_to :training_post
 
   geocoded_by :address
-  after_validation :geocode
+  after_validation :geocode, if: :address_changed?
 
   validates :address, presence: true, length: { maximum: 50 }
 end
