1955052c89c5a78fa061ec47587bf9899d9af892
matao0214 <y.kawamata0214@gmail.com>
2020-07-04 16:17:07 +0900

    training_recordコントローラを作成

---
+++ app/helpers/training_records_helper.rb
@@ -0,0 +1,2 @@
+module TrainingRecordsHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  get '/training_record', to: 'training_records#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
