62c83196b81fce4ca0139f5318074c70890e6b99
matao0214 <y.kawamata0214@gmail.com>
2020-07-07 09:42:40 +0900

    レイアウト修正

--- app/controllers/training_records_controller.rb
+++ app/controllers/training_records_controller.rb
@@ -1,10 +1,9 @@
 class TrainingRecordsController < ApplicationController
 
-
-
   def show
     @training_posts = current_user.training_posts
     @training_tasks = TrainingTask.where(training_post_id: @training_posts)
     @training_contents = TrainingContent.where(training_post_id: @training_posts)
   end
+   
 end
