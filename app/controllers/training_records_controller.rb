d585d91179ec8369bedc347f10d1437b47b744e0
matao0214 <y.kawamata0214@gmail.com>
2020-07-09 18:27:30 +0900

    ランキング機能を実装

--- app/controllers/training_records_controller.rb
+++ app/controllers/training_records_controller.rb
@@ -3,6 +3,17 @@ class TrainingRecordsController < ApplicationController
   def show
     @training_posts = current_user.training_posts
     @training_contents = TrainingContent.where(training_post_id: @training_posts)
+    
+    all_task = {
+      "球が上手く当たらない" => :task_ball_dont_hits_club,
+      "スライスする" => :task_slice,
+      "フックする" => :task_hook,
+      "飛距離が出ない" => :task_ball_dont_fly_far,
+      "クラブの番手を変えても飛距離が変わらない" => :task_flight_distance_dont_change,
+      "ダフってしまう" => :task_get_duffed, "その他" => :task_other
+    }
+    number_of_tasks = {}
+    all_task.each { |key, value| number_of_tasks[key] = @training_posts.count(value) }
+    @tasks_desc = number_of_tasks.sort_by{ | k, v | -v }.to_h
   end
-   
 end
