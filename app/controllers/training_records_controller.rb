df4f63f535ecb038b30bb496f8584d439c7be0bc
matao0214 <y.kawamata0214@gmail.com>
2020-07-11 21:58:58 +0900

    みんなの悩み画面を追加

--- app/controllers/training_records_controller.rb
+++ app/controllers/training_records_controller.rb
@@ -3,16 +3,17 @@ class TrainingRecordsController < ApplicationController
   def index
     @training_posts = TrainingPost.all
     @training_contents = TrainingContent.all
-    rank_tasks
+    @user = User.all
+    rank_tasks(@training_posts)
   end
   
   def show
     @training_posts = current_user.training_posts
     @training_contents = TrainingContent.where(training_post_id: @training_posts)
-    rank_tasks
+    rank_tasks(@training_posts)
   end
 
-  def rank_tasks
+  def rank_tasks(training_posts)
     all_task = {
       "球が上手く当たらない" => :task_ball_dont_hits_club,
       "スライスする" => :task_slice,
@@ -22,7 +23,7 @@ class TrainingRecordsController < ApplicationController
       "ダフってしまう" => :task_get_duffed, "その他" => :task_other
     }
     number_of_tasks = {}
-    all_task.each { |key, value| number_of_tasks[key] = @training_posts.count(value) }
+    all_task.each { |key, value| number_of_tasks[key] = training_posts.count(value) }
     @tasks_desc = number_of_tasks.sort_by{ | k, v | -v }.to_h
   end
 end
