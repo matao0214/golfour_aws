47bc584639ca03d8726b0fc341778427fa3fd891
matao0214 <y.kawamata0214@gmail.com>
2020-07-15 15:03:31 +0900

    Update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -8,10 +8,8 @@ workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
+preload_app!
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port        ENV.fetch("PORT") { 3000 }
-
 # Specifies the `environment` that Puma will run in.
 #
 rackup      DefaultRackup
