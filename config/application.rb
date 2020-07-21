aa809f48a4f6fccbc631205d3f45198da350b27f
matao0214 <y.kawamata0214@gmail.com>
2020-07-21 20:58:11 +0900

    タイムゾーンを環境変数に変更

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Golfour
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.time_zone = 'Tokyo'
+    config.time_zone = ENV['TZ']
     config.assets.initialize_on_precompile = false
   end
 end
