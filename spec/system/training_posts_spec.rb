46e647ae216d15ec0bba6efc24f12aaac36a02c1
matao0214 <y.kawamata0214@gmail.com>
2020-07-28 20:30:34 +0900

    テスト作成

---
+++ spec/system/training_posts_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe '練習記録投稿機能', type: :system do
+  describe '投稿一覧表示機能' do
+    before do
+      #ユーザーAを作成しておく
+      user_a = FactoryBot.create(:user, nickname: 'ユーザーA', email: 'a@example.com')
+      #作成者がユーザーAである練習記録を作成しておく
+      FactoryBot.create(:training_post, training_impression: 'RSpecテスト', user: user_a)
+    end
+    context 'ユーザーAがログインしているとき' do
+      before do
+        #ユーザーAでログインする
+        visit login_path
+        fill_in 'session_email', with: 'a@example.com'
+        fill_in 'session_password', with: 'password'
+        click_button 'ログインする'
+      end
+      it 'ユーザーAが作成した練習記録が表示される' do
+        #作成済の練習記録が画面上に表示されていることを確認
+        expect(page).to have_content 'RSpecテスト'
+      end
+    end
+  end
+end
