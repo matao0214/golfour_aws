29229d6038fd6924bb5bb7e88b45ab427268383d
matao0214 <y.kawamata0214@gmail.com>
2020-07-31 15:00:48 +0900

    Update test_spec.rb

--- spec/system/test_spec.rb
+++ spec/system/test_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe '練習記録投稿機能', type: :system do
   it 'ユーザーAが作成した練習記録が表示される' do
     #作成済の練習記録が画面上に表示されていることを確認
-    visit r_path
+    visit root_path
     expect(1 + 1).to eq 2
   end
 end
