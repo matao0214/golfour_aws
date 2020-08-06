707e689a6e99f00bf0f40f4a805dcb638b6ed69f
matao0214 <y.kawamata0214@gmail.com>
2020-08-06 14:46:22 +0900

    DBをMysqlに変更

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2020_07_08_113555) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "likes", force: :cascade do |t|
+  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "training_post_id"
     t.bigint "user_id"
     t.datetime "created_at", null: false
@@ -25,7 +22,7 @@ ActiveRecord::Schema.define(version: 2020_07_08_113555) do
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
-  create_table "spots", force: :cascade do |t|
+  create_table "spots", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "address", limit: 50, null: false
     t.float "latitude", null: false
     t.float "longitude", null: false
@@ -35,16 +32,16 @@ ActiveRecord::Schema.define(version: 2020_07_08_113555) do
     t.index ["training_post_id"], name: "index_spots_on_training_post_id"
   end
 
-  create_table "training_contents", force: :cascade do |t|
-    t.integer "training_time"
-    t.integer "training_hits"
+  create_table "training_contents", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "training_time", null: false
+    t.integer "training_hits", null: false
     t.integer "training_post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "training_posts", force: :cascade do |t|
-    t.text "training_impression"
+  create_table "training_posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "training_impression", limit: 255
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -58,15 +55,14 @@ ActiveRecord::Schema.define(version: 2020_07_08_113555) do
     t.index ["user_id"], name: "index_training_posts_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "nickname", null: false
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", limit: 10, null: false
     t.string "email", null: false
     t.string "password_digest", null: false
     t.string "golf_reki"
-    t.string "goal"
+    t.string "goal", limit: 50
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "likes", "training_posts"
