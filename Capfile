d7ca5bb8fe7808414a9a0ecbf2251d304654efe5
matao0214 <y.kawamata0214@gmail.com>
2020-08-27 13:29:27 +0900

    Capistrano関連を設定

--- Capfile
+++ Capfile
@@ -1,38 +1,15 @@
-# Load DSL and set up stages
-require "capistrano/setup"
+require 'capistrano/setup'
+require 'capistrano/deploy'
+require 'capistrano/rbenv' 
+require 'capistrano/bundler'
+require 'capistrano/rails/assets' 
+require 'capistrano/rails/migrations' 
+# taskを記述したファイルを読み込むよう設定。場所と拡張子を指定。
+Dir.glob('lib/capistrano/tasks/*.rb').each { |r| import r }
 
-# Include default deployment tasks
-require "capistrano/deploy"
-
-# Load the SCM plugin appropriate to your project:
-#
-# require "capistrano/scm/hg"
-# install_plugin Capistrano::SCM::Hg
-# or
-# require "capistrano/scm/svn"
-# install_plugin Capistrano::SCM::Svn
-# or
-require "capistrano/scm/git"
-install_plugin Capistrano::SCM::Git
-
-# Include tasks from other gems included in your Gemfile
-#
-# For documentation on these, see for example:
-#
-#   https://github.com/capistrano/rvm
-#   https://github.com/capistrano/rbenv
-#   https://github.com/capistrano/chruby
-#   https://github.com/capistrano/bundler
-#   https://github.com/capistrano/rails
-#   https://github.com/capistrano/passenger
-#
-# require "capistrano/rvm"
-# require "capistrano/rbenv"
-# require "capistrano/chruby"
-# require "capistrano/bundler"
-# require "capistrano/rails/assets"
-# require "capistrano/rails/migrations"
-# require "capistrano/passenger"
-
-# Load custom tasks from `lib/capistrano/tasks` if you have any defined
-Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
+# 初期設定
+# require "capistrano/setup"
+# require "capistrano/deploy"
+# require "capistrano/scm/git"
+# install_plugin Capistrano::SCM::Git
+# Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,65 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.14.1"
+# capistranoのバージョン固定
+lock '3.14.1'
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+# デプロイするアプリケーション名
+set :application, 'golfour'
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# cloneするgitのレポジトリ
+set :repo_url, 'git@github.com:matao0214/golfour_aws.git'
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+# deployするブランチ。デフォルトはmasterなのでなくても可。
+set :branch, 'master'
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+# deploy先のディレクトリ。 
+set :deploy_to, '/var/www/rails/golfour'
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+# シンボリックリンクをはるファイル。
+set :linked_files, fetch(:linked_files, []).push('config/settings.yml')
 
-# Default value for :pty is false
-# set :pty, true
+# シンボリックリンクをはるフォルダ。
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+# 保持するバージョンの個数。過去５つまで履歴を保存。
+set :keep_releases, 5
 
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+# rubyのバージョン
+set :rbenv_ruby, '2.5.1'
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+#出力するログのレベル。
+set :log_level, :debug
 
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    invoke 'unicorn:restart'
+  end
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+  desc 'Create database'
+  task :db_create do
+    on roles(:db) do |host|
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:create'
+        end
+      end
+    end
+  end
 
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+  desc 'Run seed'
+  task :seed do
+    on roles(:app) do
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:seed'
+        end
+      end
+    end
+  end
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+    end
+  end
+end
