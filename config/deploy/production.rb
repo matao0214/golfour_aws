4d91ee999daacc65e4f535bd6fa32f748099157b
matao0214 <y.kawamata0214@gmail.com>
2020-09-04 22:04:59 +0900

    Updating settings

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,13 @@
 # EC2サーバーのIP、EC2サーバーにログインするユーザー名、サーバーのロールを記述
 server '54.150.171.208', user: 'yuki', roles: %w[app db web]
 
-# デプロイするサーバーにsshログインする鍵の情報を記述
-set :ssh_options, keys: '~/.ssh/go_key_rsa'
+# CircleCIによる自動デプロイ前はこちらを採用
+# set :ssh_options, keys: '~/.ssh/go_key_rsa'
+
+# CircleCIのGUIで設定した環境変数を使ってSSH接続
+set :ssh_options, {  
+  keys: ["#{ENV.fetch('PRODUCTION_SSH_KEY')}"],  
+  forward_agent: true,  
+  auth_methods: %w[publickey],  
+  port: 33333,  
+}  
