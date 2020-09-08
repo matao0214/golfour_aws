b470c58abf87053dcdb0945f42a9cf8b7709485f
matao0214 <y.kawamata0214@gmail.com>
2020-09-08 12:39:22 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,8 @@ server '54.150.171.208', user: 'yuki', roles: %w[app db web]
 
 # CircleCIのGUIで設定した環境変数を使ってSSH接続
 set :ssh_options, {
-  keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
+  keys: ['~/.ssh/go.pem']
+  # keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
   forward_agent: true,
   auth_methods: %w[publickey]
 }
