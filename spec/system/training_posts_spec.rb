e7c4f397bea77495ea12c1f4bd2304be534b1880
matao0214 <y.kawamata0214@gmail.com>
2020-09-16 10:29:58 +0900

    letを使って共通化

--- spec/system/training_posts_spec.rb
+++ spec/system/training_posts_spec.rb
@@ -1,23 +1,20 @@
 require 'rails_helper'
 
 describe '練習記録投稿機能', type: :system do
-  describe '投稿一覧表示機能' do
-    before do
-      # ユーザーAを作成しておく
-      user_a = FactoryBot.create(:user, nickname: 'ユーザーA', email: 'a@example.com')
-      # 作成者がユーザーAである練習記録を作成しておく
-      FactoryBot.create(:training_post, training_impression: 'RSpecテスト', user: user_a)
-    end
+  let(:user_a) { FactoryBot.create(:user, nickname: 'ユーザーA', email: 'a@example.com') }
+  let!(:training_post_a) { FactoryBot.create(:training_post, training_impression: 'RSpecテスト', user: user_a) }
+
+  before do
+    visit login_path
+    fill_in 'session_email', with: login_user.email
+    fill_in 'session_password', with: login_user.password
+    click_button 'ログインする'
+  end
+
+  describe '一覧表示機能' do
     context 'ユーザーAがログインしているとき' do
-      before do
-        # ユーザーAでログインする
-        visit login_path
-        fill_in 'session_email', with: 'a@example.com'
-        fill_in 'session_password', with: 'password'
-        click_button 'ログインする'
-      end
+      let(:login_user) { user_a }
       it 'ユーザーAが作成した練習記録が表示される' do
-        # 作成済の練習記録が画面上に表示されていることを確認
         expect(page).to have_content 'RSpecテスト'
       end
     end
