7f9b4b57a32d3f9a338778c934692a6ec9c1d2ed
matao0214 <y.kawamata0214@gmail.com>
2020-09-18 02:44:36 +0900

    SystemSpecを追加

---
+++ spec/system/create_user_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+describe 'ユーザー機能', type: :system do
+  let!(:user_a) { FactoryBot.create(:user, email: 'test1@example.com') }
+  before do
+    visit new_user_path
+    fill_in 'ユーザー名', with: nickname
+    fill_in 'メールアドレス', with: email
+    fill_in 'パスワード', with: password
+    fill_in 'パスワード(確認)', with: password_digest
+    select '未経験', from: 'ゴルフ歴'
+    fill_in '目標', with: 'テストをパスする'
+    click_button '登録する'
+  end
+
+  describe 'ユーザー新規登録' do
+    context '必須項目を入力しているとき' do
+      let(:nickname) { 'テストユーザー' }
+      let(:email) { 'test2@example.com' }
+      let(:password) { 'password' }
+      let(:password_digest) { 'password_digest' }
+      it '正常に登録される' do
+        expect(page).to have_selector '.alert-success', text: 'ユーザー「テストユーザー」を登録しました。'
+      end
+    end
+
+    context '必須項目を入力していないとき' do
+      let(:nickname) { '' }
+      let(:email) { '' }
+      let(:password) { '' }
+      let(:password_digest) { '' }
+      it 'エラーが表示される' do
+        within '#error_explanation' do
+          expect(page).to have_content 'パスワードを入力してください'
+          expect(page).to have_content 'ユーザー名を入力してください'
+          expect(page).to have_content 'メールアドレスを入力してください'
+        end
+      end
+    end
+
+    context '既に登録されているメールアドレスを入力したとき' do
+      let(:nickname) { 'テストユーザー' }
+      # 冒頭で予め作成したユーザーのemailと同じ値を入力
+      let(:email) { 'test1@example.com' }
+      let(:password) { 'password' }
+      let(:password_digest) { 'password_digest' }
+      it 'エラーが表示される' do
+        within '#error_explanation' do
+          expect(page).to have_content 'メールアドレスはすでに存在します'
+        end
+      end
+    end
+  end
+end
